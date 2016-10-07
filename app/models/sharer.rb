f84c63a5b40a713f21cda6134cde03ea75440ffa
Erik <erikwjonsson@gmail.com>
2016-10-07 22:34:10 +0200

    Added validations in Sharer model for sharer signup details.

--- app/models/sharer.rb
+++ app/models/sharer.rb
@@ -1,2 +1,14 @@
 class Sharer < ApplicationRecord
+  validate :check_email # Checks that email is not blank and that it is a valid email adress.
+		#validates :email, uniqueness: true, uniqueness: { message: "has already been registered." }
+  validates :name, presence: true, presence: { message: " is needed." }
+  validates :city, presence: true, presence: { message: " is needed." }
+
+  def check_email
+    if email.blank?
+    	errors.add(:email, " is required.")
+    else
+    	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    end
+  end
 end
